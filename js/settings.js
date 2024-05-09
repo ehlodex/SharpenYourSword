d1bd90aa1b32a84e69ed09a0efa2bc1edd9a2afe
Joshua Burkholder <ehlodex@users.noreply.github.com>
2024-05-09 09:28:29 -0400

    Add files via upload

--- js/settings.js
+++ js/settings.js
@@ -18,21 +18,25 @@ function delVpoints(player) {
     p1_vpoints--;
     if (p1_vpoints < 0) { p1_vpoints = 0; };
     document.getElementById("p1_vpoints").innerText = p1_vpoints;
+    if (p1_vpoints == vpointsToWin) { alert(`Player 1 wins by scoring ${vpointsToWin} virtuous points!`); };
   };
   if (player =='p2') {
     p2_vpoints--;
     if (p2_vpoints < 0) { p2_vpoints = 0; };
     document.getElementById("p2_vpoints").innerText = p2_vpoints;
+    if (p2_vpoints == vpointsToWin) { alert(`Player 2 wins by scoring ${vpointsToWin} virtuous points!`); };
   };
   if (player =='p3') {
     p3_vpoints--;
     if (p3_vpoints < 0) { p3_vpoints = 0; };
     document.getElementById("p3_vpoints").innerText = p3_vpoints;
+    if (p3_vpoints == vpointsToWin) { alert(`Player 3 wins by scoring ${vpointsToWin} virtuous points!`); };
   };
   if (player =='p4') {
     p4_vpoints--;
     if (p4_vpoints < 0) { p4_vpoints = 0; };
     document.getElementById("p4_vpoints").innerText = p4_vpoints;
+    if (p4_vpoints == vpointsToWin) { alert(`Player 4 wins by scoring ${vpointsToWin} virtuous points!`); };
   };
 };
 
@@ -89,19 +93,27 @@ function delSinnies(player) {
 function addSinnies(player) {
   if (player == 'p1') {
     p1_sinnies++;
+    if (p1_sinnies > 6) { p1_sinnies = 6; };
     document.getElementById(`p1_sinnie${p1_sinnies}`).style.color = "red";
+    if (p1_sinnies == 6) { alert("Player 1 wins by defeating 6 sinnies!"); };
   };
   if (player == 'p2') {
     p2_sinnies++;
+    if (p2_sinnies > 6) { p2_sinnies = 6; };
     document.getElementById(`p2_sinnie${p2_sinnies}`).style.color = "red";
+    if (p2_sinnies == 6) { alert("Player 2 wins by defeating 6 sinnies!"); };
   };
   if (player == 'p3') {
     p3_sinnies++;
+    if (p3_sinnies > 6) { p3_sinnies = 6; };
     document.getElementById(`p3_sinnie${p3_sinnies}`).style.color = "red";
+    if (p3_sinnies == 6) { alert("Player 3 wins by defeating 6 sinnies!"); };
   };
   if (player == 'p4') {
     p4_sinnies++;
+    if (p4_sinnies > 6) { p4_sinnies = 6; };
     document.getElementById(`p4_sinnie${p4_sinnies}`).style.color = "red";
+    if (p4_sinnies == 6) { alert("Player 4 wins by defeating 6 sinnies!"); };
   };
 };
 
@@ -109,12 +121,22 @@ function toggleTournamentMode() {
   if (tournamentMode == 0) {
     tournamentMode = 1;
     document.getElementById("tournamentMode").innerText = "toggle_on";
-    document.getElementById("tournamentModeStatus").innerText = "ON (77)";
     vpointsToWin = 77;
   } else {
     tournamentMode = 0;
     document.getElementById("tournamentMode").innerText = "toggle_off";
-    document.getElementById("tournamentModeStatus").innerText = "OFF (60)";
     vpointsToWin = 60;
   }
+}
+
+function addPlayer() {
+  playerCount++;
+  if (playerCount > 4) { playerCount = 4; };
+  document.getElementById("player_count").innerText = playerCount;
+}
+
+function delPlayer() {
+  playerCount--;
+  if (playerCount < 1) { playerCount = 1; };
+  document.getElementById("player_count").innerText = playerCount;
 }
